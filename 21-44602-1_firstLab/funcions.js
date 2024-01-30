d7939f5ba11e0680b1afcd11051fe111bbc3f79f
Khan Atik Faisal <81611894+khanatik1176@users.noreply.github.com>
2024-01-30 13:48:51 +0600

    funcions.js

--- 21-44602-1_firstLab/funcions.js
+++ 21-44602-1_firstLab/funcions.js
@@ -1,6 +1,50 @@
-function greet(name)
-{
-    console.log('Hello, $(name)!');
-}
+// function greet(name)
+// {
+//     console.log(`Hello, ${name}!`);
+// }
 
-greet("Alice");
+// greet("Alice");
+
+// function sum(a,b)
+// {
+//     return a+b;
+// }
+
+// let result = sum(2,3);
+// console.log(result);
+
+// let greet = name => console.log(`Hello, ${name}!`);
+
+// greet("Bob");
+
+//Annonyms Function
+// const greet = function(name)
+// {
+//     console.log(`Hello, ${name}!`);
+// };
+
+// greet("Alice");
+
+// function operateOnNumbers(a,b,operation)
+// {
+//     return operation(a,b)
+// }
+
+// //Function to add two numbers
+// function add(x,y)
+// {
+//     return x+y;
+// }
+
+// function multiply(x,y)
+// {
+//     return x*y;
+// }
+
+// //using the higher-order function with different operations
+
+// let resultAddition = operateOnNumbers(5,3,add);
+// let resultMultiplication = operateOnNumbers(2,4,multiply);
+
+// console.log(resultAddition);
+// console.log(resultMultiplication);
